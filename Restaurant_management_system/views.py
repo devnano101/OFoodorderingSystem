0141c8d26652c4e2ff34836ca16923a733cee482
devnano101 <devopsnano@gmail.com>
2021-09-15 03:46:39 +0600

    first commit

---
+++ Restaurant_management_system/views.py
@@ -0,0 +1,9 @@
+# I have created this file - Darshan
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
